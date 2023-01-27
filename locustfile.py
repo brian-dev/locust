cb71dd86b8b24876d68e0caa43bd5bb0bde69b05
Brian Richardson <brianrichardson@Brians-MacBook-Pro.local>
2023-01-27 15:05:01 -0600

    add first test

---
+++ locustfile.py
@@ -0,0 +1,15 @@
+from locust import HttpUser, task, between
+
+
+class WebsiteTestUser(HttpUser):
+    wait_time = between(0.5, 3.0)
+
+    def on_start(self):
+        pass
+
+    def on_stop(self):
+        pass
+
+    @task(1)
+    def hello_world(self):
+        self.client.get('http://localhost:5000')
